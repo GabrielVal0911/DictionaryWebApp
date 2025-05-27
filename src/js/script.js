58ac249e0cc20e81f741dbb92c3d06de2cdd7b0a
GabrielVal0911 <gabrielvalentin091101@yahoo.com>
2025-05-27 19:45:33 +0300

    Added initial files and basic intro page elements

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// async function getP() {
+//   try {
+//     const response = await fetch(
+//       "https://api.dictionaryapi.dev/api/v2/entries/en/what"
+//     );
+//     if (response.ok) {
+//       const data = await response.json();
+//       console.log(data);
+//     } else {
+//       throw new Error("Failed to fetch data");
+//     }
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// }
+// getP();
+
+//  sa folosesc use strict
